3dfef4e498fd73c01f6d4a0907f5f5d4d4fcfef7
Ananthi A <ananthiaathi47@gmail.com>
2026-05-11 16:30:08 +0530

    Fixed Render URL in ALLOWED_HOST

--- postify_app/settings.py
+++ postify_app/settings.py
@@ -25,7 +25,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['postify-hub.onrender.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['postify-hub-p5fp.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
